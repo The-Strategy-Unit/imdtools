f3cb16e7b2e3b920be4d8c7898284128c307b250
Fran Barton <francis.barton@nhs.net>
2026-01-16 16:52:40 +0000

    ➕ Add get_imd_lookup() function

---
+++ R/get_imd_lookup.R
@@ -0,0 +1,26 @@
+#' Import ranks and deciles for all English LSOAs for the 2025 IMD
+#'
+#' @returns A tibble with columns for LSOA codes and names, LAD codes and names,
+#'  and IMD ranks and deciles
+#' @export
+get_imd_lookup <- function() {
+  base <- "https://assets.publishing.service.gov.uk/media"
+  fold <- "691dece32c6b98ecdbc500d5"
+  file <- "File_1_IoD2025_Index_of_Multiple_Deprivation.xlsx"
+
+  file.path(base, fold, file) |>
+    openxlsx2::read_xlsx(sheet = "IMD25") |>
+    tibble::as_tibble() |>
+    dplyr::rename_with(\(x) {
+      c(
+        "lsoa21cd",
+        "lsoa21nm",
+        "lad24cd",
+        "lad24nm",
+        "imd_rank",
+        "imd_decile"
+      )
+    }) |>
+    dplyr::mutate(dplyr::across(tidyselect::starts_with("imd"), as.integer)) |>
+    dplyr::mutate(dplyr::across("imd_decile", as.factor))
+}
