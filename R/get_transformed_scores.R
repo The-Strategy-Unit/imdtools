304331fe71f4d86cd6c91b67648866bcf6f9bcdf
Fran Barton <francis.barton@nhs.net>
2026-01-17 04:19:27 +0000

    ➕ Add get_overall_scores() function

--- R/get_transformed_scores.R
+++ R/get_transformed_scores.R
@@ -26,3 +26,27 @@ get_transformed_scores <- function() {
       )
     })
 }
+
+#' Download the overall IMD score for each LSOA
+#'
+#' @returns A tibble with columns for LSOA codes and names, LAD codes and names,
+#'  and the overall score for each LSOA
+#' @export
+get_overall_scores <- function() {
+  fold <- "691ded34513046b952c500bd"
+  file <- "File_5_IoD2025_Scores_for_the_Indices_of_Deprivation.xlsx"
+
+  file.path(base_gov_url(), fold, file) |>
+    openxlsx2::read_xlsx(sheet = "IoD2025 Scores") |>
+    tibble::as_tibble() |>
+    dplyr::select(seq(5)) |>
+    dplyr::rename_with(\(x) {
+      c(
+        "lsoa21cd",
+        "lsoa21nm",
+        "lad24cd",
+        "lad24nm",
+        "imd_score"
+      )
+    })
+}
